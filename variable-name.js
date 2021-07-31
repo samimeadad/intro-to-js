e50633fac77e2f1b08d696dee1b8a0fd56c05664
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-07-31 21:07:33 +0600

    vdo-16.4 variable name convention

---
+++ variable-name.js
@@ -0,0 +1,21 @@
+var name = 'Tasnuva Kawsar Farmee';
+
+// Reserved Keywords NOT allowed for variable name
+// example
+// var false = 15;
+// var delete = delete;
+
+// Name should be one word. No space allowed.Name
+// var nayok name = 'Tom Hanks';
+// var nayika name = 'Shabnoor';
+
+
+// No Quote is allowed in variable name
+// var "name" = "Farmee";
+
+var myofficeaddress = 'uttara, dhaka';
+var myofficeaddress = 'uttara, dhaka';
+var myofficeaddress = 'uttara, dhaka';
+var myofficeaddress = 'uttara, dhaka';
+var myofficeaddress = 'uttara, dhaka';
+var myofficeaddress = 'uttara, dhaka';
