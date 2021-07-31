3537caf40efdc45441b2f27f164fd02e949ea75b
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-07-31 19:39:51 +0600

    variable understood

---
+++ variable.js
@@ -0,0 +1,14 @@
+var price = 30;
+var age = 40;
+var temperature = 37;
+var isDificult = false;
+var name = 'sami';
+
+console.log(isDificult);
+console.log(name);
+
+if(isDificult == false){
+    name='farmee';
+}
+    
+console.log(name);
